62bfd012488f78ea6556b2d74558e880c8c3ad13
huija <1150555483@qq.com>
2020-06-22 16:45:47 +0800

    one partition per consumer

--- kafka/consumer/consumer.go
+++ kafka/consumer/consumer.go
@@ -13,6 +13,7 @@ import (
 )
 
 type Consumer struct {
+	key   string
 	ready chan bool
 }
 
@@ -27,17 +28,30 @@ func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
 
 func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for message := range claim.Messages() {
-		log.Printf("Message claimed: key = %s, value = %v, topic = %s, partition = %v, offset = %v", string(message.Key), string(message.Value), message.Topic, message.Partition, message.Offset)
+		log.Printf("ConsumerId: %s, Message claimed: key = %s, value = %v, topic = %s, partition = %v, offset = %v", consumer.key, string(message.Key), string(message.Value), message.Topic, message.Partition, message.Offset)
 		session.MarkMessage(message, "")
 	}
-
 	return nil
+}
+
+func (consumer Consumer) Run(ctx context.Context, wg *sync.WaitGroup, consumerGroup sarama.ConsumerGroup, topics string) {
+	wg.Add(1)
+	defer wg.Done()
+	for {
+		if err := consumerGroup.Consume(ctx, strings.Split(topics, ","), &consumer); err != nil {
+			log.Panicf("Error from consumer: %v", err)
+		}
+		if ctx.Err() != nil {
+			return
+		}
+		consumer.ready = make(chan bool, 0)
+	}
 }
 
 func main() {
 	// 中断消费后继续消费的消息有序性, 只在同一个partition内才能保证
 	broker := "localhost:9092"
-	group := "demo"
+	groupId := "demo"
 	topics := "sarama"
 	kafkaVersion := "2.4.0"
 
@@ -48,33 +62,34 @@ func main() {
 
 	config := sarama.NewConfig()
 	config.Version = version
-	// 自定义Consumer结构体,需要实现ConsumerGroupHandler接口, 并保证并发安全
-	consumer := Consumer{
-		ready: make(chan bool, 0),
-	}
+	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategySticky
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 
 	ctx, cancel := context.WithCancel(context.Background())
-	client, err := sarama.NewConsumerGroup([]string{broker}, group, config)
+	consumerGroup0, err := sarama.NewConsumerGroup([]string{broker}, groupId, config)
+	if err != nil {
+		log.Panicf("Error creating consumer group client: %v", err)
+	}
+	consumerGroup1, err := sarama.NewConsumerGroup([]string{broker}, groupId, config)
 	if err != nil {
 		log.Panicf("Error creating consumer group client: %v", err)
 	}
 
 	wg := &sync.WaitGroup{}
-	go func() {
-		wg.Add(1)
-		defer wg.Done()
-		for {
-			if err := client.Consume(ctx, strings.Split(topics, ","), &consumer); err != nil {
-				log.Panicf("Error from consumer: %v", err)
-			}
-			if ctx.Err() != nil {
-				return
-			}
-			consumer.ready = make(chan bool, 0)
-		}
-	}()
+	// 自定义Consumer结构体,需要实现ConsumerGroupHandler接口, 并保证并发安全
+	consumer0 := Consumer{
+		key:   "0号",
+		ready: make(chan bool, 0),
+	}
+	consumer1 := Consumer{
+		key:   "1号",
+		ready: make(chan bool, 0),
+	}
+	go consumer0.Run(ctx, wg, consumerGroup0, topics)
+	go consumer1.Run(ctx, wg, consumerGroup1, topics)
 
-	<-consumer.ready
+	<-consumer0.ready
+	<-consumer1.ready
 
 	sigterm := make(chan os.Signal, 1)
 	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
@@ -86,7 +101,10 @@ func main() {
 	}
 	cancel()
 	wg.Wait()
-	if err = client.Close(); err != nil {
+	if err = consumerGroup0.Close(); err != nil {
+		log.Panicf("Error closing client: %v", err)
+	}
+	if err = consumerGroup1.Close(); err != nil {
 		log.Panicf("Error closing client: %v", err)
 	}
 }
